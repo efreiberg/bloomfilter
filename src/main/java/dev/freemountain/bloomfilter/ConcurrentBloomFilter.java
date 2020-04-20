ad0c4acb96a75a58622cf7150136e8499298c9fa
Eric Freiberg <ejfreiberg@gmail.com>
2020-04-20 12:03:40 -0500

    clean up bit manipulation

--- src/main/java/dev/freemountain/bloomfilter/ConcurrentBloomFilter.java
+++ src/main/java/dev/freemountain/bloomfilter/ConcurrentBloomFilter.java
@@ -12,15 +12,18 @@ import java.util.concurrent.atomic.AtomicLongArray;
 public class ConcurrentBloomFilter implements SimpleBloomFilter {
 
     private static final int BITS_IN_LONG = 64;
+    private static final int LONG_OFFSET_BITS = 6;
+    private static final int LONG_OFFSET_MASK = BITS_IN_LONG - 1;
+    private final int MAX_NUM_BITS;
     private final AtomicLongArray longArray;
-    private final int MAX_CAPACITY;
 
     public ConcurrentBloomFilter(int capacity) {
         if (capacity < 1) {
             throw new IllegalArgumentException();
         }
-        MAX_CAPACITY = (int) Math.ceil(capacity / (double) BITS_IN_LONG);
-        longArray = new AtomicLongArray(MAX_CAPACITY);
+        int numLongs = (int) Math.ceil(capacity / (double) BITS_IN_LONG);
+        MAX_NUM_BITS = numLongs * BITS_IN_LONG;
+        longArray = new AtomicLongArray(numLongs);
     }
 
     @Override
@@ -31,10 +34,12 @@ public class ConcurrentBloomFilter implements SimpleBloomFilter {
         final List<Integer> hashValues = BloomFilterHasher.getHashValues(value);
         hashValues.stream().forEach(hashValue -> {
             boolean success;
+            int hashMod = hashValue % MAX_NUM_BITS;
             // Find array entry
-            int address = (int) (hashValue / (double) BITS_IN_LONG) % MAX_CAPACITY;
+            int address = hashMod >>> LONG_OFFSET_BITS;
             // Find bit
-            int offset = hashValue % BITS_IN_LONG;
+            int offset = hashMod & LONG_OFFSET_MASK;
+            // Isolate bit
             long mask = 1 << offset;
             do {
                 long oldValue = longArray.get(address);
@@ -54,10 +59,11 @@ public class ConcurrentBloomFilter implements SimpleBloomFilter {
         }
         final List<Integer> hashValues = BloomFilterHasher.getHashValues(value);
         return hashValues.stream().allMatch(hashValue -> {
+            int hashMod = hashValue % MAX_NUM_BITS;
             // Find array entry
-            int address = (int) (hashValue / (double) BITS_IN_LONG) % MAX_CAPACITY;
+            int address = hashMod >>> LONG_OFFSET_BITS;
             // Find bit
-            int offset = hashValue % BITS_IN_LONG;
+            int offset = hashMod & LONG_OFFSET_MASK;
             // Isolate bit
             long mask = 1 << offset;
             return (longArray.get(address) & mask) != 0;
